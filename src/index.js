4371c5038460fd7717b3ac6b85e16350cd1a44aa
mustafaerk <34791079+mustafaerk@users.noreply.github.com>
2022-04-27 13:22:49 +0300

    persist redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "Router";
-import { store } from "redux/store";
+import { persistedStore, store } from "redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "reduxjs-toolkit-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistedStore}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
-
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import middleware from "redux/middleware";
 import rootReducer from "redux/rootReducer";
+import storage from "reduxjs-toolkit-persist/lib/storage";
+import { persistStore, persistReducer } from "reduxjs-toolkit-persist";
+
+const persistConfig = {
+  key: "REDUX_TOOLKIT_PERSIST_KEY",
+  storage,
+  whitelist: ["auth", "room", "user"],
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers(rootReducer)
+);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(middleware),
 });
+
+export const persistedStore = persistStore(store);
