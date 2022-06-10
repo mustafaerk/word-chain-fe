7999f74c12c995ef8134ec5864cec56b0635aefd
mustafaerk <telemak33@gmail.com>
2022-06-10 17:35:45 +0300

    backend ip changed

--- src/constant/Varible.js
+++ src/constant/Varible.js
@@ -1,2 +1,2 @@
 export const ENV = process.env.REACT_APP_ENV;
-export const API_URL = "http://185.255.93.106:5001" || 'http://localhost:5001'; 
+export const API_URL = "https://be.linhug.com" || 'http://localhost:5001'; 
